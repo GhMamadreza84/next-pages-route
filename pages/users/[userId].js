1813243abd484e654e080f62b1f77501482b0d25
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-09 09:04:47 +0330

    i edit using userId

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -1,8 +1,8 @@
 import { useRouter } from "next/router";
 const User = () => {
   const router = useRouter();
-  console.log(router);
-  return <p>user # {router.query.userId}</p>;
+  const id = router.query.userId;
+  return <p>user # {id}</p>;
 };
 
 export default User;
