0d00c931459cbefb6fd8405028b2b6d7be6fbf6e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-10 11:11:43 +0330

    i create a students folder and in this folder i create a another folder with name [studentId] for show student details

---
+++ pages/students/[studentId]/index.js
@@ -0,0 +1,5 @@
+const Students = () => {
+  return <h1>Student Details</h1>;
+};
+
+export default Students;
---
+++ pages/students/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentsList = () => {
+  return (
+    <h1>
+      Students List
+    </h1>
+  )
+}
+
+export default StudentsList
