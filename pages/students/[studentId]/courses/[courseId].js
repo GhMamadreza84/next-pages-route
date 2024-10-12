a95837641543a8bae82051f5fa665e08e8c15bcb
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-12 09:16:41 +0330

    i use course id and student id in courseId file

--- pages/students/[studentId]/courses/[courseId].js
+++ pages/students/[studentId]/courses/[courseId].js
@@ -2,8 +2,11 @@ import { useRouter } from "next/router";
 const Course = () => {
   const router = useRouter();
   const { courseId, studentId } = router.query;
-  console.log("CourseId: ", courseId, " |", "StudentId :", studentId);
-  return <h1>one of the student's course</h1>;
+  return (
+    <h1>
+      details of courses #{courseId} for student #{studentId}
+    </h1>
+  );
 };
 
 export default Course;
