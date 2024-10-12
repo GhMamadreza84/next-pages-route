9bc260c95a94547414ee79276625b45b7b4743d1
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-12 09:15:24 +0330

    i get ids with useRouter hook and test it in courseId.js file

--- pages/students/[studentId]/courses/[courseId].js
+++ pages/students/[studentId]/courses/[courseId].js
@@ -1,4 +1,8 @@
+import { useRouter } from "next/router";
 const Course = () => {
+  const router = useRouter();
+  const { courseId, studentId } = router.query;
+  console.log("CourseId: ", courseId, " |", "StudentId :", studentId);
   return <h1>one of the student's course</h1>;
 };
 
