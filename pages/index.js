ea734046d7600530de0f83b77316da9a80119e75
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-12 09:43:29 +0330

    i now underestand the Link tag

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-     <h1>Ghmamadreza.ir</h1>
+      <h1>Ghmamadreza.ir</h1>
+      <ul>
+        <li>
+          <Link href="/courses">Go to courses page</Link>
+        </li>
+        <li>
+          <Link href="/users">Go to users page</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
