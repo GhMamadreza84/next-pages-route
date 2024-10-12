44239997f5904213959cd94bcd93fee4f68b758d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-12 10:30:33 +0330

    i with link tag i create dynamic route in users folder

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,5 +1,19 @@
- const index = () => {
-  return <p>users</p>;
+import Link from "next/link";
+
+const index = () => {
+  return (
+    <>
+      <h1>Users List</h1>
+      <ul>
+        <li>
+          <Link href="/users/1">User 1</Link>
+        </li>
+        <li>
+          <Link href="/users/2">User 2</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default index;
