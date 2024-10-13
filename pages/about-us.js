9f8dd2ab1f3545943f8249bb68c8b8278b8bbf6e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-13 10:51:36 +0330

    i create a aboutus.module.css and add style to this page for test

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,10 +1,6 @@
-
+import styles from "../styles/Aboutus.module.css";
 const About = () => {
-  return (
-    <h1>
-      About Us
-    </h1>
-  )
-}
+  return <h1 className={styles.text}>About Us</h1>;
+};
 
-export default About
+export default About;
