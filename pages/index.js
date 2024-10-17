0062e459fe975d7493e7cbf067ef09933e99f4da
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-17 09:36:29 +0330

    i now fetch data from api in statick site generation

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,28 @@
 import styles from "../styles/Home.module.css";
 
-export default function Home({ data }) {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <div className={styles.container}>
       <h1>Ghmamadreza.ir</h1>
-      <h2>{data}</h2>
+      <ul>
+        {posts.map((post) => (
+          <>
+            <li key={post.id}>{post.title}</li> <br></br>
+          </>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export async function getStaticProps() {
   //  fetch data from api
-  const data = "Mohammadreza Ghadiri";
-
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await data.json();
   return {
     props: {
-      data,
+      posts: res,
     },
   };
 }
