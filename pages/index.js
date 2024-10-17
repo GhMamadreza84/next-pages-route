cf6c9c659279aab8cf3cfbd2d804ef19ab5b1b3d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-17 09:04:30 +0330

    i edit index js main route

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Link from "next/link";
-import { useRouter } from "next/router";
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <div className={styles.container}>
       <h1>Ghmamadreza.ir</h1>
