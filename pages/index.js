300426cf4b1c741ee54db72a4f315a636c052800
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-17 09:24:12 +0330

    i underestand how i can get data in ssg

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div className={styles.container}>
       <h1>Ghmamadreza.ir</h1>
+      <h2>{data}</h2>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  //  fetch data from api
+  const data = "Mohammadreza Ghadiri";
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
