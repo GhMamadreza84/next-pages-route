fc0ebd72fa972a1a7b4192239b668d15629d89f7
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-19 12:10:43 +0330

    i now use getStaticPath function for define dynamic route id

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -4,6 +4,23 @@ const User = () => {
 
 export default User;
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { userId: "1" } },
+      { params: { userId: "2" } },
+      { params: { userId: "3" } },
+      { params: { userId: "4" } },
+      { params: { userId: "5" } },
+      { params: { userId: "6" } },
+      { params: { userId: "7" } },
+      { params: { userId: "8" } },
+      { params: { userId: "9" } },
+      { params: { userId: "10" } },
+    ],
+  };
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const res = await fetch(`jsonplaceholder.typicode/users/${params.userId}`);
