d830e81f7467023b14b9c49994cea0f4cf6af999
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-20 12:10:34 +0330

    i now create a dynamic path

--- pages/users/[userId].js
+++ pages/users/[userId].js
@@ -11,19 +11,15 @@ const User = ({ data }) => {
 export default User;
 
 export async function getStaticPaths() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await res.json();
+  const paths = data.map((user) => ({
+    params: {
+      userId: user.id.toString(),
+    },
+  }));
   return {
-    paths: [
-      { params: { userId: "1" } },
-      { params: { userId: "2" } },
-      { params: { userId: "3" } },
-      { params: { userId: "4" } },
-      { params: { userId: "5" } },
-      { params: { userId: "6" } },
-      { params: { userId: "7" } },
-      { params: { userId: "8" } },
-      { params: { userId: "9" } },
-      { params: { userId: "10" } },
-    ],
+    paths,
     fallback: false,
   };
 }
@@ -34,6 +30,7 @@ export async function getStaticProps(context) {
     `https://jsonplaceholder.typicode.com/users/${params.userId}`
   );
   const data = await res.json();
+
   console.log(params);
   return {
     props: {
