8f5d62408fadd5a24220f315943922794450f23c
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-26 10:45:48 +0330

    i now underestand how to use hookSWR for data fetching

--- pages/todos/index.js
+++ pages/todos/index.js
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const url = "http://localhost:4000/todos";
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Todos = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:4000/todos")
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/todos")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data));
+  // }, []);
+  const { data, error } = useSWR(url, fetcher);
   return (
     <div>
-      {data.length ? (
+      {/* {data.length ? (
         <ul>
           {data.map((item) => (
             <li key={item.id}>
@@ -21,6 +26,11 @@ const Todos = () => {
         </ul>
       ) : (
         <h1>Loading ...</h1>
+      )} */}
+      {data ? (
+        data.map((todo) => <h3 key={todo.id}>{todo.title}</h3>)
+      ) : (
+        <p>Loading ...</p>
       )}
     </div>
   );
