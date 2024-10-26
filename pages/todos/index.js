08e33d5574a3c6b688dada28cd6d7343f01dc0fe
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-26 10:53:09 +0330

    i edit useSWR hook in todos route

--- pages/todos/index.js
+++ pages/todos/index.js
@@ -5,28 +5,9 @@ const url = "http://localhost:4000/todos";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Todos = () => {
-  // const [data, setData] = useState([]);
-  // useEffect(() => {
-  //   fetch("http://localhost:4000/todos")
-  //     .then((res) => res.json())
-  //     .then((data) => setData(data));
-  // }, []);
   const { data, error } = useSWR(url, fetcher);
   return (
     <div>
-      {/* {data.length ? (
-        <ul>
-          {data.map((item) => (
-            <li key={item.id}>
-              {item.title}
-              <br></br>
-              <br></br>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <h1>Loading ...</h1>
-      )} */}
       {data ? (
         data.map((todo) => <h3 key={todo.id}>{todo.title}</h3>)
       ) : (
