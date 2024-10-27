7bfb5a8ae34e2846cdb72ef961c20e0fd130563b
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-10-27 08:26:12 +0330

    i edit todoId route and have bug now

--- pages/todos/[todoId].js
+++ pages/todos/[todoId].js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoDetails = ({todo}) => {
+const TodoDetails = ({ todo }) => {
+  const [data, setData] = useState(todo || {});
+
+  const updateHandler = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:4000/todos/${id}`);
+      if (!res.ok) throw new Error("Failed to fetch data.");
+      const updatedData = await res.json();
+      setData(updatedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <div>
-      {todo.id} - {todo.title}
+      <h2>
+        {data.id} - {data.title}
+      </h2>
+      <h3>{`${data.completed}`}</h3>
+      <button onClick={() => updateHandler(data.id)}>Update</button>
     </div>
   );
 };
@@ -12,9 +29,10 @@ export default TodoDetails;
 
 export async function getServerSideProps(context) {
   const { params } = context;
-  const res = await fetch(`http://localhost:4000/todos/${params.todoId}`);
-  const data = await res.json();
 
+  const res = await fetch(`http://localhost:4000/todos/${params.todoId}`);
+
+  const data = await res.json();
   return {
     props: {
       todo: data,
